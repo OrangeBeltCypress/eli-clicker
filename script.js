3857e96b81aff29b06d8b5457d87bbd1c4f40f3f
OrangeBeltCypress <153779903+OrangeBeltCypress@users.noreply.github.com>
2024-05-29 19:23:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const char = document.querySelector(".char")
+const clicks = document.querySelector(".clicks")
+
+
+let amount = 0;
+
+
+char.addEventListener("mousedown", ()=>{
+    amount++
+    clicks.innerHTML = "Clicks: " + amount
+    char.style.transform = "scale(1.1)"
+})
+
+
+char.addEventListener("mouseup", ()=>{
+    char.style.transform = "scale(1)"
+})
+
